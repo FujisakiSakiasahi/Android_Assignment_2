c3c012463a5f71a0496247b92a01fbce188a472f
FujisakiSakiasahi <90231449+FujisakiSakiasahi@users.noreply.github.com>
2022-11-18 21:04:41 +0800

    menu setup complete

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.assignment2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.assignment2.databinding.ActivityMainBinding;
 
@@ -18,5 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //create menu object & place it on then action bar
+        getMenuInflater().inflate(R.menu.menu_layout, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_setting:
+
+                break;
+            case R.id.menu_scoreboard:
+
+                break;
+            case R.id.menu_taktaulagi:
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
