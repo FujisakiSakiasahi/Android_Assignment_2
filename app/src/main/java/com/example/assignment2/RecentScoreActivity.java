171f1341d1287d4ccd8a017cda12683aeff99db7
FujisakiSakiasahi <90231449+FujisakiSakiasahi@users.noreply.github.com>
2022-11-26 14:13:36 +0800

    Comment for all activities except MainActivity

--- app/src/main/java/com/example/assignment2/RecentScoreActivity.java
+++ app/src/main/java/com/example/assignment2/RecentScoreActivity.java
@@ -20,17 +20,23 @@ public class RecentScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recent_scores);
 
+        //set up view binding
         binding = RecentScoresBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //set onclick listener for the button to end this activity
         binding.buttonBack.setOnClickListener(view -> finish());
 
+        //get the pass in intent
         Intent intent = getIntent();
 
+        //retrieve the scoreList from the pass in intent parcelable
         scoreList = (ScoreList) intent.getParcelableExtra("SCORE_LIST");
 
+        //set the adapter for the list item view
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, scoreList.getRecentScores());
 
+        //set the adapter to the list item view
         binding.listviewRecentScores.setAdapter(adapter);
 
     }
--- app/src/main/java/com/example/assignment2/SettingActivity.java
+++ app/src/main/java/com/example/assignment2/SettingActivity.java
@@ -21,6 +21,7 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
 
+        //set up view binding
         settingBinding = SettingBinding.inflate(getLayoutInflater());
         setContentView(settingBinding.getRoot());
 
@@ -31,38 +32,80 @@ public class SettingActivity extends AppCompatActivity {
         //if exists load status else create file
         checkSettingStatusFile();
 
+        //set the onclick listener to the set default button
         settingBinding.buttonSettingSetDefault.setOnClickListener(view -> {
+            //set the settings to default
             setDefaultSettingStatus();
+            //save the setting
             savingSettingStatus();
+            //toast the success message
             Toast.makeText(this, getString(R.string.setting_toast_set_default), Toast.LENGTH_SHORT).show();
         });
 
+        //set the onclick listener to the save button
         settingBinding.buttonSettingSave.setOnClickListener(view -> {
+            //save the setting
             savingSettingStatus();
+            //toast the success message
             Toast.makeText(this, getString(R.string.setting_toast_save), Toast.LENGTH_SHORT).show();
         });
 
+        //set the onclick listener to the close button
+        //use to close the current activity
         settingBinding.buttonSettingClose.setOnClickListener(view -> {
             finish();
         });
     }//end of onCreate
 
+    /**
+     * checkSettingStatusFile: <br/>
+     * - check if the setting status file is created or not <br/>
+     * - if exists load status else create file
+     */
+    private void checkSettingStatusFile() {
+        //check if file exist
+        if(new File(getApplicationContext().getFilesDir(), SETTING_FILE_NAME).exists()){
+            //load data setting
+            setCurrentSettingStatus(fileHandler.loadData());
+        }else{
+            //set the setting to the default status
+            setDefaultSettingStatus();
+            //create and save the file
+            savingSettingStatus();
+        }
+    }
+
+    /**
+     * setCurrentSettingStatus: <br/>
+     * - use to load the data settings
+     *
+     * @param status the settings that need to be loaded
+     */
+    private void setCurrentSettingStatus(String[] status) {
+        String[] stat = new String[status.length];
+
+        for(int i = 0; i<status.length; i++){
+            stat[i] = status[i].split(" = ")[1];
+        }
+
+        settingBinding.checkboxSplashScreen.setChecked(Boolean.parseBoolean(stat[0]));
+        settingBinding.switchSoundEffect.setChecked(Boolean.parseBoolean(stat[1]));
+    }
+
+    /**
+     * setDefaultSettingStatus: <br/>
+     * - set the settings to default value
+     */
     private void setDefaultSettingStatus() {
         //set the settings view into default setting status
         settingBinding.checkboxSplashScreen.setChecked(false);
         settingBinding.switchSoundEffect.setChecked(true);
     }
 
-    private void checkSettingStatusFile() {
-        if(new File(getApplicationContext().getFilesDir(), SETTING_FILE_NAME).exists()){
-            //load data setting
-            setCurrentSettingStatus(fileHandler.loadData());
-        }else{
-            setDefaultSettingStatus();
-            savingSettingStatus();
-        }
-    }
-
+    /**
+     * savingsettingStatus: <br/>
+     * - use to save the settings into the file
+     */
     private void savingSettingStatus() {
         //create string array that need to be saved
         String[] saveString = new String[2];
@@ -74,16 +117,5 @@ public class SettingActivity extends AppCompatActivity {
         //save the setting status
         fileHandler.setData(saveString);
         fileHandler.saveData();
-    }
-
-    private void setCurrentSettingStatus(String[] status) {
-        String[] stat = new String[status.length];
-
-        for(int i = 0; i<status.length; i++){
-            stat[i] = status[i].split(" = ")[1];
-        }
-
-        settingBinding.checkboxSplashScreen.setChecked(Boolean.parseBoolean(stat[0]));
-        settingBinding.switchSoundEffect.setChecked(Boolean.parseBoolean(stat[1]));
     }
 }
